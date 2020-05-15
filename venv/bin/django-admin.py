78b3a31b63491793300fb7070b4994e676b63927
M Nour <mnkanout@gmail.com>
2020-05-15 14:11:29 +0000

    Before updating DjangoBlog

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mohammadnourkanout/Desktop/python_workspace/blog/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
