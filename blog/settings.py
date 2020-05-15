3bed6ca47a796a87eb0665db570e449a3358a741
MNkanout <mnkanout@gmail.com>
2020-05-15 18:04:50 +0200

    On branch master 	modified:   blog/settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't3%h*sd(u_^_sew5=loz@y=_wef^+3j(qykcefq0q1q@%&^%#^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 debug = os.environ.get('DEBUG')
