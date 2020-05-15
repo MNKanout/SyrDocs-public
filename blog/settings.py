457b9298beaea9e3042dfe2439eea9366babcbe9
MNkanout <mnkanout@gmail.com>
2020-05-15 15:58:58 +0200

    Removed secret_key, and fixed debug mode

--- blog/settings.py
+++ blog/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't3%h*sd(u_^_sew5=loz@y=_wef^+3j(qykcefq0q1q@%&^%#^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+debug = os.environ.get('DEBUG')
+if debug == 'True':
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['syrwegian.com']
 
 
 # Application definition
