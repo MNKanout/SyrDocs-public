7c748b578300840f9d95ec9e4c6ee5f059e48f62
MNkanout <mnkanout@gmail.com>
2020-06-25 17:42:48 +0200

    Custom navbar and footer styling. Adding static DIRS

--- blog/urls.py
+++ blog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     # Defualt urls 
@@ -26,4 +28,4 @@ urlpatterns = [
     # Users app urls
     path('users/',include('users.urls')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
